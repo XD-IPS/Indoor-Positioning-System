01b8288e4ba31c5ebea48a727f765ba0df41f180
Li Yilong <lobsterliyilong@gmail.com>
2017-05-08 00:57:38 +0800

    A start

--- TestCpp.cpp
+++ TestCpp.cpp
@@ -1,1 +1,20 @@
-#include <iostem>
+# include<iostream>
+# include<opencv2/core/core.hpp>
+# include<opencv2/highgui/highgui.hpp>
+# include<opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	Mat img = imread("C:\\Users\\Lobster\\Desktop\\Test.png");
+
+	resize(img, img, Size(500, 500));
+
+	imshow("image", img);
+
+	waitKey();
+	destroyAllWindows();
+	return 0;
+}
